689391bd4fa5dfbc7632c0bff106a9c94c53cb32
uniquesonu <21bcs10550@cuchd.in>
2024-07-16 01:33:42 +0530

    chore: Remove unused Hero component and update Preview component styling

--- components/Preview.tsx
+++ components/Preview.tsx
@@ -4,11 +4,12 @@ import { SandpackProvider, SandpackPreview } from "@codesandbox/sandpack-react";
 
 const Preview = ({ files }: { files: any }) => {
   return (
-    <div className="h-full p-2 bg-gray-800">
+    <div className="h-full p-2 bg-gray-900">
       <h2 className="text-lg font-bold mb-2">Preview</h2>
-      <div className="h-full border border-gray-00 rounded-md overflow-hidden">
-        <SandpackProvider files={files} template="static">
-          <SandpackPreview />
+      <div className="h-full border border-gray-300 rounded-md overflow-hidden">
+        <SandpackProvider files={files} template="static" >
+        <SandpackPreview className="w-full h-full" />
+
         </SandpackProvider>
       </div>
     </div>
